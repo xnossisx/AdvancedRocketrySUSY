0600e5272039e916213366ac1d3e55639de35636
MrToffel <marvineckhardt04092003@gmail.com>
2024-06-02 20:17:58 +0200

    Update BiomeHandler.java
    water generation code currently not used. I want it to have its own satellite or on/off setting maybe

--- src/main/java/zmaster587/advancedRocketry/util/BiomeHandler.java
+++ src/main/java/zmaster587/advancedRocketry/util/BiomeHandler.java
@@ -1,20 +1,95 @@
 package zmaster587.advancedRocketry.util;
 
+import net.minecraft.block.BlockLiquid;
+import net.minecraft.init.Biomes;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.fml.common.Loader;
+import zmaster587.advancedRocketry.api.IPlanetaryProvider;
+import zmaster587.advancedRocketry.api.dimension.IDimensionProperties;
+import zmaster587.advancedRocketry.dimension.DimensionManager;
+import zmaster587.advancedRocketry.dimension.DimensionProperties;
 import zmaster587.advancedRocketry.network.PacketBiomeIDChange;
 import zmaster587.libVulpes.network.PacketHandler;
 import zmaster587.libVulpes.util.HashedBlockPosition;
 
+import static zmaster587.advancedRocketry.util.AstronomicalBodyHelper.getAverageTemperature;
+
 public class BiomeHandler {
 
+
+    //public static boolean is_watercontrol_loaded = Loader.isModLoaded("watercontrol");
 
     public static void changeBiome(World world, Biome biomeId, BlockPos pos) {
         Chunk chunk = world.getChunkFromBlockCoords(pos);
 
         Biome biome = world.getBiome(pos);
+
+
+        /*
+        // spread and evaporate water - !!only possible if watercontrol is loaded!!
+        if (is_watercontrol_loaded) {
+            if (world.provider instanceof IPlanetaryProvider) {
+
+
+
+                // for ocean/river biomes generate water
+                if (biome == Biome.getBiome(0) ||biome == Biome.getBiome(7)) {
+                    int sealvl = world.getSeaLevel();
+                    BlockPos yy = world.getHeight(pos);
+
+
+                    while ((!world.getBlockState(yy.down()).isOpaqueCube() && yy.getY() > 0)
+                            && !(world.getBlockState(yy.down()).getBlock() == Blocks.WATER)) {
+                        yy = yy.down();
+                    }
+                    if (yy.getY() <= sealvl) {
+                        world.setBlockState(yy, Blocks.WATER.getDefaultState());
+                    }
+                }
+
+
+
+
+
+
+                //spread water
+                if (DimensionManager.getInstance().getDimensionProperties(world.provider.getDimension()).water_can_exist) {
+
+                    int sealvl = world.getSeaLevel();
+                    BlockPos yy = new BlockPos(pos.getX(), sealvl, pos.getZ());
+
+
+                    while ((!world.getBlockState(yy.down()).isOpaqueCube() && yy.getY() > 0)
+                            && !(world.getBlockState(yy.down()).getBlock() == Blocks.WATER)
+                    ) {
+                        yy = yy.down();
+                        if (
+                                world.getBlockState(yy.east()).getBlock() == Blocks.WATER
+                                        || world.getBlockState(yy.north()).getBlock() == Blocks.WATER
+                                        || world.getBlockState(yy.west()).getBlock() == Blocks.WATER
+                                        || world.getBlockState(yy.south()).getBlock() == Blocks.WATER
+                        )
+                            world.setBlockState(yy, Blocks.WATER.getDefaultState());
+
+                    }
+
+                }
+                //evaporate water
+                else {
+                    // replace top water block with air
+                    BlockPos top_block = world.getHeight(pos).down();
+                    if (world.getBlockState(top_block).getBlock() == Blocks.WATER || world.getBlockState(top_block).getBlock() == Blocks.FLOWING_WATER) {
+                        world.setBlockState(top_block, Blocks.AIR.getDefaultState());
+
+                    }
+                }
+            }
+        }
+        */
 
         if (biome == biomeId)
             return;
@@ -35,6 +110,9 @@ public class BiomeHandler {
         } catch (IndexOutOfBoundsException e) {
             e.printStackTrace();
         }
+
+
+
 
         PacketHandler.sendToNearby(new PacketBiomeIDChange(chunk, world, new HashedBlockPosition(pos)), world.provider.getDimension(), pos, 256);
     }
