ba84642d77db4ed6d9ec9db9a5355d2d7fb5066a
Stanislav <39829332+StannisMod@users.noreply.github.com>
2024-09-04 17:27:10 +0300

    Fix/motor render (#30)
    Fixed rocket motor render issues

--- src/main/java/zmaster587/advancedRocketry/client/render/RendererBrokenPart.java
+++ src/main/java/zmaster587/advancedRocketry/client/render/RendererBrokenPart.java
@@ -1,11 +1,15 @@
 package zmaster587.advancedRocketry.client.render;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.Vec3i;
 import zmaster587.advancedRocketry.backwardCompat.WavefrontObject;
 import zmaster587.advancedRocketry.client.ClientProxy;
 import zmaster587.advancedRocketry.tile.TileBrokenPart;
+import zmaster587.libVulpes.block.BlockFullyRotatable;
 
 public class RendererBrokenPart extends TileEntitySpecialRenderer<TileBrokenPart> {
 
@@ -18,6 +22,23 @@ public class RendererBrokenPart extends TileEntitySpecialRenderer<TileBrokenPart
         if (!name.equals("air")) {
             GlStateManager.pushMatrix();
             GlStateManager.translate((float) x, (float) y, (float) z);
+
+            if (tile.getBlockType() instanceof BlockFullyRotatable) {
+                IBlockState state = tile.getWorld().getBlockState(tile.getPos());
+                EnumFacing facing = state.getBlock().getActualState(state, tile.getWorld(), tile.getPos()).getValue(BlockFullyRotatable.FACING);
+                Vec3i dir = facing.getDirectionVec();
+                GlStateManager.translate(0.5F, 0.5F, 0.5F);
+                if (dir.getY() > 0) {
+                    GlStateManager.rotate(180.0F, 1.0F, 0.0F, 0.0F);
+                }
+                if (dir.getX() != 0) {
+                    GlStateManager.rotate(90.0F, 0.0F, 0.0F, -dir.getX());
+                }
+                if (dir.getZ() != 0) {
+                    GlStateManager.rotate(90.0F, dir.getZ(), 0.0F, 0.0F);
+                }
+                GlStateManager.translate(-0.5F, -0.5F, -0.5F);
+            }
 
             if (destroyStage >= 0) {
                 this.bindTexture(DESTROY_STAGES[destroyStage]);
